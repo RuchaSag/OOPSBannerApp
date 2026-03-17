493dd8e3e5f1403485ca1667dc9694c79e8950ae
RuchaSag <rs8728@srmist.edu.in>
2026-03-17 13:40:28 +0530

    Coded UC3

--- UC3.java
+++ UC3.java
@@ -1,11 +1,12 @@
-class UC2{
+public class UC3 {
     public static void main(String[] args){
-        System.out.println("   ***   " + "    ***   " + " ******   " + "    *****");
-        System.out.println(" **   ** " + "  **   ** " + " **    ** " + "  **     ");
-        System.out.println("**     **" + " **     **" + " **     **" + " **      ");
-        System.out.println("**     **" + " **     **" + " **    ** " + "   ***** ");
-        System.out.println("**     **" + " **     **" + " ******   " + "       **");
-        System.out.println(" **   ** " + "  **   ** " + " **       " + "  **   **");
-        System.out.println("   ***   " + "    ***   " + " **       " + "   ***** ");  
+        System.out.println(String.join(" ", "   ***   " + "    ***   " + " ******   " + "    *****"));
+        System.out.println(String.join(" ", " **   ** " + "  **   ** " + " **    ** " + "  **     "));
+        System.out.println(String.join(" ", "**     **" + " **     **" + " **     **" + " **      "));
+        System.out.println(String.join(" ", "**     **" + " **     **" + " **    ** " + "   ***** "));
+        System.out.println(String.join(" ", "**     **" + " **     **" + " ******   " + "       **"));
+        System.out.println(String.join(" ", " **   ** " + "  **   ** " + " **       " + "  **   **"));
+        System.out.println(String.join(" ", "   ***   " + "    ***   " + " **       " + "   ***** "));  
     }
+    
 }
